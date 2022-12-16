34aab923b439d2855f5ec254b58d3ef539e73694
Alex Johnson <alex.johnson293@gmail.com>
2022-12-16 14:30:55 -0800

    add Order controller/views

--- BakeryOrders/Controllers/OrderController.cs
+++ BakeryOrders/Controllers/OrderController.cs
@@ -4,9 +4,25 @@ using System.Collections.Generic;
 
 namespace BakeryOrders.Controllers
 {
-  public class OrderController : Controller
-  {
+  public class OrdersController : Controller
+  { 
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
 
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("Vendor", vendor);
+      return View(model);
+    }
   
   }
 }
